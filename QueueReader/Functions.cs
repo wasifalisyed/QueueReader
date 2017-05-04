81f433e43c3f3a0b3cab5f136da961406f51e0fa
Wasif.Ali <Wasif.Ali@WASIF-ALI>
2017-05-04 17:21:09 +0500

    commit

---
+++ QueueReader/Functions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace QueueReader
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("messagequeue")] string message, [Queue("result")]CloudQueue resultQueue)
+        {
+
+            CloudQueueMessage msg = new CloudQueueMessage(message);
+            resultQueue.AddMessage(msg);
+
+        }
+    }
+}
